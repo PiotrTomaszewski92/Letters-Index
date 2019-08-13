1c11b6c2908002504483011fafe217f8ceb329af
Piotr Tomaszewski <piotrekha15@gmail.com>
2019-08-13 18:38:49 +0200

    implemented test with specified text

--- src/test/java/AlgorithmTest.java
+++ src/test/java/AlgorithmTest.java
@@ -1,10 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.*;
+
 import static org.junit.Assert.*;
 
 public class AlgorithmTest {
@@ -26,6 +24,28 @@ public class AlgorithmTest {
         mappedWords.put('l',ala);
 
         assertEquals(mappedWords,algorithm.getMappedWords("Ala"));
+
+    }
+
+    @Test
+    public void getMappedWordsWithLongerText(){
+
+        Map<Character,Set<String>> mappedWords = new TreeMap<>();
+        mappedWords.put('a', new TreeSet<>(Arrays.asList("ala","javie","kota","ma")));
+        mappedWords.put('d',new TreeSet<>(Arrays.asList("koduje")));
+        mappedWords.put('e',new TreeSet<>(Arrays.asList("javie","koduje")));
+        mappedWords.put('i',new TreeSet<>(Arrays.asList("javie")));
+        mappedWords.put('j',new TreeSet<>(Arrays.asList("javie","koduje")));
+        mappedWords.put('k',new TreeSet<>(Arrays.asList("koduje","kot","kota")));
+        mappedWords.put('l',new TreeSet<>(Arrays.asList("ala")));
+        mappedWords.put('m',new TreeSet<>(Arrays.asList("ma")));
+        mappedWords.put('o',new TreeSet<>(Arrays.asList("koduje","kot","kota")));
+        mappedWords.put('t',new TreeSet<>(Arrays.asList("kot","kota")));
+        mappedWords.put('u',new TreeSet<>(Arrays.asList("koduje")));
+        mappedWords.put('v',new TreeSet<>(Arrays.asList("javie")));
+        mappedWords.put('w',new TreeSet<>(Arrays.asList("w")));
+
+        assertEquals(mappedWords,algorithm.getMappedWords("ala ma kota, kot koduje w Javie kota"));
 
     }
 }
