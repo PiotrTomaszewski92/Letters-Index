f19cd91d513793d3f4811a373930d96f1f9acc61
Piotr Tomaszewski <piotrekha15@gmail.com>
2019-08-13 18:52:41 +0200

    separate code to private methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,20 +4,29 @@ import java.util.Set;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Algorithm algorithm = new Algorithm();
-
-        System.out.println("~~~Welcome in program, which index each letter in text and map to the words in which it occurs ~~~\n");
-
-        System.out.println("Enter the text: ");
-        String text = new Scanner(System.in).nextLine();
-
-        Map<Character, Set<String>> map = algorithm.getMappedWords(text);
-
-        map.entrySet().stream().forEach(key-> {
+    private void printMap(Map<Character, Set<String>> wordsMap) {
+        wordsMap.entrySet().stream().forEach(key-> {
             String values = key.getValue().toString().replace("[","").replace("]","").trim();
 
             System.out.println(key.getKey()+": "+ values);
         });
+    }
+
+    private String getText(){
+        System.out.println("Enter the text: ");
+        return new Scanner(System.in).nextLine();
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Algorithm algorithm = new Algorithm();
+
+        System.out.println("~~~Welcome in program, which index each letter in text and map to the words in which it occurs ~~~\n");
+
+        String text = main.getText();
+
+        Map<Character, Set<String>> wordsMap = algorithm.getMappedWords(text);
+
+        main.printMap(wordsMap);
     }
 }
