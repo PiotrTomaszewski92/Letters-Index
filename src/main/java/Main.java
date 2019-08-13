cde2b2d75b1404fd146521948a91706776fc0c0e
Piotr Tomaszewski <piotrekha15@gmail.com>
2019-08-13 18:47:26 +0200

    implemented Main class where program is starting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,23 @@
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Algorithm algorithm = new Algorithm();
+
+        System.out.println("~~~Welcome in program, which index each letter in text and map to the words in which it occurs ~~~\n");
+
+        System.out.println("Enter the text: ");
+        String text = new Scanner(System.in).nextLine();
+
+        Map<Character, Set<String>> map = algorithm.getMappedWords(text);
+
+        map.entrySet().stream().forEach(key-> {
+            String values = key.getValue().toString().replace("[","").replace("]","").trim();
+
+            System.out.println(key.getKey()+": "+ values);
+        });
     }
 }
