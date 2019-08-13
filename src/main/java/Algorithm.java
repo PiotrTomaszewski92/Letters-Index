899c6bf221580d68dc7374d2be3e65a10c398eaa
Piotr Tomaszewski <piotrekha15@gmail.com>
2019-08-13 18:34:50 +0200

    implemented main algorithm to return mapped words

--- src/main/java/Algorithm.java
+++ src/main/java/Algorithm.java
@@ -1,9 +1,35 @@
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Algorithm {
 
+    private final String regexOnlyWords = "[^a-z']+";
+
+    private String[] splitToWordsArray(String text){
+        return text.toLowerCase().split(regexOnlyWords);
+    }
+
+    private Set<String> addWordToSetCollection( Set<String> distinctWords, String word){
+        if (distinctWords == null)
+            distinctWords = new TreeSet<>();
+
+        distinctWords.add(word);
+        return distinctWords;
+    }
+
     public Map<Character, Set<String>> getMappedWords(String text){
-        return null;
+        Map<Character, Set<String>> wordsMap = new TreeMap<>();
+
+        String[] words = splitToWordsArray(text);
+        Set<String> wordsSet = new HashSet<>(Arrays.asList(words));
+
+        for (String word : wordsSet) {
+            for (int i = 0; i < word.length(); i++) {
+                Character key = word.charAt(i);
+                Set<String> wordsSetCollection = addWordToSetCollection(wordsMap.get(key), word);
+                wordsMap.put(key, wordsSetCollection);
+            }
+        }
+
+        return wordsMap;
     }
 }
